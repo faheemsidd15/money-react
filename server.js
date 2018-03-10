2a69d77b7dc5f3db90e68ed2619ee897ce19bc7c
Mohammad Al-Abbasi <sheikhhmo@gmail.com>
2018-03-09 21:33:02 -0500

    changed server.js and added routing functionality

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-    {id: 4, firstName: 'Joe', lastName: 'Shmoe'}
-  ];
+const routes = require('./routes/index.js');
+app.use('/', routes);
 
-  res.json(customers);
-});
+// app.get('/api/customers', (req, res) => {
+//   const customers = [
+//     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+//     {id: 4, firstName: 'Joe', lastName: 'Shmoe'}
+//   ];
+
+//   res.json(customers);
+// });
 
 const port = 5000;
 
